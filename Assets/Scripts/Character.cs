3a692b827cc67655bfa84364c51d65ae4a27f30c
Krupoderov Mikhail <krupoderov.mikhail@gmail.com>
2020-10-07 19:48:22 +0500

    Added character's animation.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -13,6 +13,14 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float jumpForce = 15.0F;
 
+    private bool isGrounded = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value);  }
+    }
+
     new private Rigidbody2D rigidbody;
     private Animator animator;
     private SpriteRenderer sprite;
@@ -24,10 +32,17 @@ public class Character : MonoBehaviour
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
 
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
     private void Update()
     {
+        State = CharState.Idle;
+
         if (Input.GetButton("Horizontal")) Run();
-        if (Input.GetButtonDown("Jump")) Jump();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump(); // Если на земле и нажат пробел, персонаж прыгает
     }
 
     private void Run()
@@ -37,14 +52,33 @@ public class Character : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
 
         sprite.flipX = direction.x < 0.0F;
+
+        State = CharState.Run;
     }
 
+
+    /* Прыжок персонажа */
     private void Jump() 
     {
         rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+
+        State = CharState.Jump;
+    }
+
+    /* Находится ли персонаж на земле */
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);  // Коллайдер под персонажем(сфера)
+
+        isGrounded = colliders.Length > 1; // Если коллайдер больше 1, то мы на земле
+    }
+
+    /* Состояния игрока */
+    public enum CharState
+    {
+        Idle, // 0
+        Run,  // 1
+        Jump  // 2
     }
 }
 
-/** 25:52 
-* https://www.youtube.com/watch?v=CAPVBTKk3Ww 
- *//
